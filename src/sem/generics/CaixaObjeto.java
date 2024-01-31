58c5f2eafbe25d6c1b41d80d95b763f6b6f3a15a
ilsantos <ilsantos3@stefanini.com>
2024-01-31 15:13:18 -0300

    Implementação sem usar o Generics.

---
+++ src/sem/generics/CaixaObjeto.java
@@ -0,0 +1,16 @@
+package sem.generics;
+
+public class CaixaObjeto {
+
+	private Object obj;
+
+	
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+}
---
+++ src/sem/generics/Main.java
@@ -0,0 +1,27 @@
+package sem.generics;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CaixaObjeto caixa1 = new CaixaObjeto();
+		caixa1.setObj(2.3);   // double -> Double (autoboxing)
+		
+		Double valor1 = (Double) caixa1.getObj();   // Casting - não recomendado
+		System.out.println(valor1);
+		
+		
+		CaixaObjeto caixa2 = new CaixaObjeto();
+		caixa2.setObj("Olá"); 
+		
+		String valor2 = (String) caixa2.getObj();   // Casting - não recomendado
+		System.out.println(valor2);
+		
+		
+		CaixaObjeto caixa3 = new CaixaObjeto();
+		caixa3.setObj(2.3); 
+		
+		Integer valor3 = (Integer) caixa2.getObj();   // Vai lançar exceção por tentar fazer casting de Double para Integer
+		
+	}
+}
